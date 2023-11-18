ab36b14a3f65fb65b4c6978315932004c0072400
vicdant1 <dantjv@outlook.com>
2023-11-18 13:54:41 -0300

    creating imdmarket - web2

---
+++ java/imdmarket/src/main/java/com/imdmarket/imdmarket/ImdmarketApplication.java
@@ -0,0 +1,13 @@
+package com.imdmarket.imdmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImdmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImdmarketApplication.class, args);
+	}
+
+}
---
+++ java/imdmarket/src/test/java/com/imdmarket/imdmarket/ImdmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imdmarket.imdmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImdmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
