10118b2294551d9caf903671f7b1f793c148fc96
vicdant1 <dantjv@outlook.com>
2024-09-30 11:59:00 -0300

    c++ directory

---
+++ cpp/random-imperative-and-unreasonable-study/code/console-programs/input/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    int x;
+    cout << "Type a number: ";
+    cin >> x;
+
+    cout << "Typed number: " << x << endl;
+}
---
+++ cpp/random-imperative-and-unreasonable-study/code/console-programs/sum/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int a, b, sum;
+	cin >> a;
+	cin >> b;
+	
+	sum = a+b;
+	cout << sum << endl;
+
+
+}
